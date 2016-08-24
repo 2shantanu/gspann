c241be854d708e22d632f2996d1b9c6ecc0a2b01
Shantanu <Shantanu>
2016-08-24 12:28:45 +0530

    message

---
+++ src/test/java/com/a/b/CTest.java
@@ -0,0 +1,29 @@
+package com.a.b;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CTest {
+
+	@Test
+	public void t1(){
+		System.out.println("Hi I am Test t1");
+		Assert.assertTrue(true, "");
+		
+	}
+	
+	@Test
+	public void t2(){
+		System.out.println("Hi I am Test t2");
+		Assert.assertTrue(false, "");
+		
+	}
+	
+	@Test
+	public void t3(){
+		System.out.println("Hi I am Test t3");
+		Assert.assertTrue(true, "");
+		
+	}
+	
+}
